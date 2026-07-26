c14ca26b5e19ee6e6373ba65d83e12066f8b7695
Umesh Vashist <us781819@gmail.com>
2026-07-26 11:48:01 +0530

    DevBoard 0.2

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 import { redirect } from "next/navigation"
 
-export default function RootPage() {
-  redirect("/dashboard")
+export default async function RootPage(props: {
+  searchParams?: Promise<any> | any
+}) {
+  const resolvedParams = props.searchParams instanceof Promise 
+    ? await props.searchParams 
+    : props.searchParams;
+
+  const queryString = resolvedParams 
+    ? new URLSearchParams(resolvedParams).toString() 
+    : "";
+
+  redirect(`/dashboard${queryString ? `?${queryString}` : ""}`)
 }
